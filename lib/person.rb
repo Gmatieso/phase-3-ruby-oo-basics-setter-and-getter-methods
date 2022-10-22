f22d9a17a9b7ba17c8f23ef1641c57a0bb2da3c8
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-10-22 21:03:03 +0300

    :wrench: Setter Getter Job Method

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,26 @@
 class Person
+    #setter name method 
+    def name=(name)
+        #instance variable name 
+        @name =name
+    end
+
+    #getter name method 
+    def name
+        @name
+    end
+
+    #setter job method 
+    def job=(job)
+         #instance variable job 
+        @job =job
+    end
+
+    #getter job method 
+    def job
+        @job
+    end
+    
+
 
 end
